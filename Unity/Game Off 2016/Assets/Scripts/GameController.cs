9319aeb4e868d41a470b3d035b153b640c033e3a
Matthew Chase <antishow@gmail.com>
2016-11-03 19:37:42 -0400

    Made GameController a persistent singleton, organized a few things, turned player into a prefab

--- Unity/Game Off 2016/Assets/Scripts/GameController.cs
+++ Unity/Game Off 2016/Assets/Scripts/GameController.cs
@@ -3,12 +3,19 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
-
-	// Use this for initialization
+	static GameController instance = null;
+	
 	void Start () {
-	
+		if (instance != null && instance != this) {
+			Destroy (gameObject);
+			print ("Duplicate game controller self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
