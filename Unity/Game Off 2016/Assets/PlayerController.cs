c4a3b75954bce9d598070be0157e1b06165e2464
Matthew Chase <antishow@gmail.com>
2016-11-03 14:19:18 -0400

    Added some basic stuff just to ensure people can clone/build/run

---
+++ Unity/Game Off 2016/Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rb;
+
+	public float Speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dX = Input.GetAxis("Horizontal");
+		float dY = Input.GetAxis("Vertical");
+		Vector2 V = new Vector2(dX, dY);
+
+		rb.velocity = V * Speed;
+	}
+}
