cb8c714089e3f697d3463b1870e175da9fdb9512
Matthew Chase <antishow@gmail.com>
2016-11-05 13:22:17 -0400

    Basic "room" controller for camera. Just jump-cuts right now, could be better

---
+++ Unity/Game Off 2016/Assets/RoomMarker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomMarker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.tag == "Player"){
+			print("Go to room " + this.gameObject.name);
+			Camera.main.transform.position = new Vector3(
+				this.transform.position.x,
+				this.transform.position.y,
+				Camera.main.transform.position.z
+			);
+		}
+	}
+}
