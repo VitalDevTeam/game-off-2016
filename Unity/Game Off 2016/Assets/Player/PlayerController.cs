2cb952a6542a841861b53cfceb93f39cab70e96a
Matthew Chase <antishow@gmail.com>
2016-11-05 11:29:21 -0400

    Added a function to PlayerController to determine the direction the player is moving

--- Unity/Game Off 2016/Assets/Player/PlayerController.cs
+++ Unity/Game Off 2016/Assets/Player/PlayerController.cs
@@ -1,10 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PlayerController : MonoBehaviour {
+	public const string UP = "up";
+	public const string RIGHT = "right";
+	public const string DOWN = "down";
+	public const string LEFT = "left";
+	
+	private string _direction = DOWN;
+
 	private Rigidbody2D rb;
 
 	public float Speed = 3.0f;
+
+	public string GetDirection(){
+		Vector2 V = rb.velocity;
+		string ret = "";
+
+		if(V.x == 0 && V.y == 0){
+			ret = _direction;
+		} else {
+			if(Mathf.Abs(V.y) > Mathf.Abs(V.x)){
+				if(V.y > 0){
+					ret = UP;
+				} else {
+					ret = DOWN;
+				}
+			} else {
+				if(V.x > 0){
+					ret = RIGHT;
+				} else {
+					ret = LEFT;
+				}
+			}
+		}
+
+		return ret;
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -18,5 +51,6 @@ public class PlayerController : MonoBehaviour {
 		Vector2 V = new Vector2(dX, dY);
 
 		rb.velocity = V.normalized * Speed;
+		_direction = GetDirection();
 	}
 }
