24eee5e4b88f279c18ba0c679ef045b0c763e8e0
dmensinger <dan@vtldesign.com>
2016-11-15 18:30:45 -0500

    Enemy takes basic damage

--- Unity/Game Off 2016/Assets/Enemy/EnemyController.cs
+++ Unity/Game Off 2016/Assets/Enemy/EnemyController.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int currentHealth;                   // The current health the enemy has.
+    public int damagePerShot = 20;              // The damage inflicted by each bullet.
+
+    bool isDead;                                // Whether the enemy is dead.
+
 	// Use this for initialization
-	void Start () {
+	void Start () { 
+		
+        // Setting the current health when the enemy first spawns.
+        currentHealth = startingHealth;
 	
 	}
 	
@@ -14,6 +23,30 @@ public class EnemyController : MonoBehaviour {
 	}
 
 	void Shoot(){
-		Debug.LogFormat("Oh my God, you shot {0}! You Bastard!", gameObject.name);
+
+		// If the enemy is dead...
+        if(isDead)
+            // no need to take damage so exit the function.
+            return;
+
+        // Reduce the current health by the amount of damage sustained.
+        currentHealth -= damagePerShot;
+
+        // If the current health is less than or equal to zero...
+        if(currentHealth <= 0) {
+            // ... the enemy is dead.
+            Death ();
+        }
+
+		Debug.LogFormat("Oh my God, you shot the {0}!", gameObject.name);
 	}
+
+	void Death ()
+    {
+        // The enemy is dead.
+        isDead = true;
+
+		Debug.LogFormat("Enemy is Dead", gameObject.name);
+    }
+
 }
