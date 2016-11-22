13ed4102cbcd391e00b968a7da08e439683f2494
Matthew Chase <antishow@gmail.com>
2016-11-22 07:08:28 -0500

    Added Shotgun

---
+++ Unity/Game Off 2016/Assets/Weapons/Shotgun/ShotgunController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotgunController : WeaponController {
+
+	public int PelletCount = 8;
+	public float Spread = 0.041667f; 
+
+	public override void StartAttack(Vector3 Position, float Heading){
+		int i;
+		float halfArc = Spread * 0.5f;
+		float pelletHeading;
+
+		for(i=0; i<PelletCount; i++){
+			pelletHeading = Heading + Mathf.Lerp(-halfArc, halfArc, (float)i / (PelletCount-1));
+			while(pelletHeading > 1){ pelletHeading -= 1; }
+			while(pelletHeading < 0){ pelletHeading += 1; }
+
+			FireBullet(Position, pelletHeading);
+		}
+	}
+}
