58d181d4a43882230e9520cd4d79b01b41ec7144
dmensinger <dan@vtldesign.com>
2016-11-22 20:46:54 -0500

    Enemy follower script that chases player non stop

---
+++ Unity/Game Off 2016/Assets/Enemy/EnemyFollower.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFollower : MonoBehaviour {
+	private Transform player; 
+	public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player").transform;
+	}
+
+	void Update () {
+		Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position, transform.TransformDirection(Vector3.up));
+        transform.rotation = new Quaternion(0, 0, rotation.z, rotation.w);
+		transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+	}
+	
+}
