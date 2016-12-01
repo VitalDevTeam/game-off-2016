e5d1b423e430bfb5f1cd1438e30e3abacd8662f6
Matthew Chase <antishow@gmail.com>
2016-12-01 12:42:01 -0500

    Added SFX for weapon mod pickup

--- Unity/Game Off 2016/Assets/Powerups/WeaponPowerup.cs
+++ Unity/Game Off 2016/Assets/Powerups/WeaponPowerup.cs
@@ -3,10 +3,26 @@ using System.Collections;
 
 public class WeaponPowerup : MonoBehaviour {
 	public GameObject Weapon;
+	public AudioClip PickupSound;
+	private AudioSource audioSource;
+	private SpriteRenderer sprite;
+	private Collider2D collider;
+
+	void Awake(){
+		audioSource = GetComponent<AudioSource>();
+		sprite = GetComponent<SpriteRenderer>();
+		collider = GetComponent<Collider2D>();
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
 		PlayerController player = GameObject.Find("Player").GetComponent<PlayerController>();
 		player.Weapon = Weapon;
-		Destroy(gameObject);
+		collider.enabled = false;
+		sprite.enabled = false;
+
+		audioSource.clip = PickupSound;
+		audioSource.Play();
+
+		Destroy(gameObject, PickupSound.length);
 	}
 }
