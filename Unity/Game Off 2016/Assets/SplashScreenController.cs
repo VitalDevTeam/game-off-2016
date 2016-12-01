2638b06ef6ff88ce83a5214cb056997528f4ab26
Matthew Chase <antishow@gmail.com>
2016-12-01 09:45:09 -0500

    Added Vital splash screen

---
+++ Unity/Game Off 2016/Assets/SplashScreenController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashScreenController : MonoBehaviour {
+	public void Complete(){
+		SceneManager.LoadScene("Title Screen");
+	}
+}
